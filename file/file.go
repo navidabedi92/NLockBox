962cc27ed6bb598e347f56eea140d62f1a9d0464
intelejentboy <32359711+intelejentboy@users.noreply.github.com>
2025-05-31 21:47:00 +0330

    add edit command

---
+++ file/file.go
@@ -0,0 +1,44 @@
+package file
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+type Secret struct {
+	Username string
+	Password string
+}
+
+func Write(path string, text string) {
+	file, err := os.OpenFile(path, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+	defer file.Close()
+
+	// Write to the file
+	if _, err := file.WriteString(text + "\n"); err != nil {
+		fmt.Println("Error writing to file:", err)
+	} else {
+		fmt.Println("Successfully appended to file.")
+	}
+}
+
+func ReadFile(path string) []Secret {
+	var secrets []Secret
+
+	data, _ := os.ReadFile(path)
+	lines := strings.Split(string(data), "\n")
+	for _, line := range lines {
+		if line != "" {
+			up := strings.Split(line, "		")
+			secrets = append(secrets, Secret{Username: up[0], Password: up[1]})
+		}
+	}
+
+	return secrets
+
+}
--- main.go
+++ main.go
@@ -6,37 +6,49 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+
+	"github.com/navidabedi92/NLockBox.git/encryption"
+	"github.com/navidabedi92/NLockBox.git/file"
 )
 
 func main() {
+	secret := []byte("myverystrongpasswordo32bitlength")
+	var secretFilePath string
 	if len(os.Args) < 2 {
 		log.Fatal("Incorrect Command. Usage: add|del|list [flags]")
 		os.Exit(1)
 	}
 
 	localAppData := filepath.Join(os.Getenv("LOCALAPPDATA"), "NLockBox")
-
+	secretFilePath = filepath.Join(localAppData, "secrets.txt")
 	_, err := os.Stat(localAppData)
-
 	if err != nil {
 		os.Mkdir(localAppData, 0700)
-		os.Create(filepath.Join(localAppData, "secrets.txt"))
+		os.Create(secretFilePath)
 	}
 
 	addCmd := flag.NewFlagSet("add", flag.ExitOnError)
+	editCmd := flag.NewFlagSet("edit", flag.ExitOnError)
 	delCmd := flag.NewFlagSet("del", flag.ExitOnError)
 	_ = flag.NewFlagSet("list", flag.ExitOnError)
 
 	addUsername := addCmd.String("username", "", "username of the secret")
 	addPassword := addCmd.String("password", "", "password of the secret")
 
+	// editUsername := editCmd.String("username", "", "username of the secret")
+	// editPassword := editCmd.String("password", "", "password of the secret")
+
 	delUsername := delCmd.String("username", "", "username of the secret")
 
 	switch os.Args[1] {
 	case "add":
 		addCmd.Parse(os.Args[2:])
-		fmt.Println(*addUsername)
-		fmt.Println(*addPassword)
+		encrypted, _ := encryption.Encrypt(secret, []byte(*addPassword))
+		file.Write(secretFilePath, *addUsername+"		"+string(encrypted))
+	case "edit":
+		editCmd.Parse(os.Args[2:])
+		encrypted, _ := encryption.Encrypt(secret, []byte(*addPassword))
+		file.Write(secretFilePath, *addUsername+"		"+string(encrypted))
 	case "del":
 		delCmd.Parse(os.Args[2:])
 		fmt.Println(*delUsername)
